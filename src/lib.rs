b85b6b31966bb9ffc9aa945312cacfd2d39bc6f8
hannses <hannses@example.com>
2023-10-02 17:09:02 +0200

    initial release

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,36 @@
-#![feature(const_precise_live_drops)]
+#![allow(incomplete_features)]
+#![feature(adt_const_params)]
 
-pub enum COption<T: Sized>{
+use core::marker::ConstParamTy;
+
+#[cfg(feature = "self_rust_tokenize")]
+use self_rust_tokenize::{SelfRustTokenize, quote, QuoteToTokens};
+
+#[derive(Copy, Clone, PartialEq, Eq, ConstParamTy)]
+pub enum COption<T: Copy>{
     Some(T),
     None
 }
 
-impl<T: Sized> COption<T>{
+#[cfg(feature = "self_rust_tokenize")]
+impl<T: Copy + QuoteToTokens> SelfRustTokenize for COption<T> {
+    fn append_to_token_stream(&self, token_stream: &mut self_rust_tokenize::proc_macro2::TokenStream) {
+        let iter = match self{
+            COption::Some(s) => {
+                let s = quote!(#s);
+                quote!(COption::Some(#s))
+            },
+            COption::None => quote!(COption::None),
+        };
+        token_stream.extend(iter)
+    }
+}
 
-    pub const fn into_std(self) -> Option<T>{
+impl<T: Copy> COption<T>{
+
+    pub const fn into_std(&self) -> Option<T>{
         match self{
-            COption::Some(t) => Option::Some(t),
+            COption::Some(t) => Option::Some(*t),
             COption::None => Option::None,
         }
     }
